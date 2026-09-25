e10cb848bf55e1d57ef12bb55469fa5ddacc3065
Jordy <jordyarayajiron@gmail.com>
2026-09-25 12:00:43 -0600

    Parametrizacion alu opcodes

--- Datapath_RISCV/rtl/core/riscv_pkg.sv
+++ Datapath_RISCV/rtl/core/riscv_pkg.sv
@@ -1,8 +1,9 @@
 package riscv_pkg;
+//Anchos
     localparam int XLEN        = 32;           // ancho de palabra
     localparam int REG_ADDR_W  = 5;            // 32 registros -> 5 bits
     localparam logic [31:0] RESET_VECTOR = 32'h0000_0000;  // spec: inicio en 0x0
-
+//Opcodes
     localparam logic [6:0] OP_R      = 7'b0110011; // add, sub, sll, slt, ...
     localparam logic [6:0] OP_I_ALU  = 7'b0010011; // addi, slli, slti, ...
     localparam logic [6:0] OP_LOAD   = 7'b0000011; // lw
@@ -12,5 +13,17 @@ package riscv_pkg;
     localparam logic [6:0] OP_JALR   = 7'b1100111; // jalr
     localparam logic [6:0] OP_LUI    = 7'b0110111; // lui   (lo genera 'li')
     localparam logic [6:0] OP_AUIPC  = 7'b0010111; // auipc (lo genera 'la'/'call')
+//Alu instructons
+    localparam logic [3:0] ALU_ADD    = 4'b0000;
+    localparam logic [3:0] ALU_SLL    = 4'b0001;
+    localparam logic [3:0] ALU_SLT    = 4'b0010;  // con signo
+    localparam logic [3:0] ALU_SLTU   = 4'b0011;  // sin signo
+    localparam logic [3:0] ALU_XOR    = 4'b0100;
+    localparam logic [3:0] ALU_SRL    = 4'b0101;
+    localparam logic [3:0] ALU_OR     = 4'b0110;
+    localparam logic [3:0] ALU_AND    = 4'b0111;
+    localparam logic [3:0] ALU_SUB    = 4'b1000;
+    localparam logic [3:0] ALU_SRA    = 4'b1101;
+    localparam logic [3:0] ALU_PASS_B = 4'b1111;  // resultado = operando B (lui)
 
 endpackage
