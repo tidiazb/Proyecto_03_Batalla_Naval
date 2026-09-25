690a544c85bbf7580694e42cd3d1b20aaf7bf6cc
Jordy <jordyarayajiron@gmail.com>
2026-09-25 11:58:17 -0600

    Anchos de datos

--- Datapath_RISCV/rtl/core/riscv_pkg.sv
+++ Datapath_RISCV/rtl/core/riscv_pkg.sv
@@ -1,4 +1,6 @@
 package riscv_pkg;
-
+    localparam int XLEN        = 32;           // ancho de palabra
+    localparam int REG_ADDR_W  = 5;            // 32 registros -> 5 bits
+    localparam logic [31:0] RESET_VECTOR = 32'h0000_0000;  // spec: inicio en 0x0
 
 endpackage
